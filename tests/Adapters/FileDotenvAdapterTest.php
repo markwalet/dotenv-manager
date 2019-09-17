0865618ff18b638697ba97690b6efe4bf19e0d8b
= <m.walet@code14.nl>
2019-09-17 13:14:51 +0200

    Remove setup method

--- tests/Adapters/FileDotenvAdapterTest.php
+++ tests/Adapters/FileDotenvAdapterTest.php
@@ -6,50 +6,40 @@ use PHPUnit\Framework\TestCase;
 class FileDotenvAdapterTest extends TestCase
 {
 
-    /**
-     * Sets up the fixture, for example, open a network connection.
-     * This method is called before a test is executed.
-     */
-    protected function setUp()
-    {
-        file_put_contents(__DIR__.'/.env.testing', 'Sample content');
-        file_put_contents(__DIR__.'/.env.other', 'Other content');
-    }
-
-    /**
-     * Tears down the fixture, for example, close a network connection.
-     * This method is called after a test is executed.
-     */
-    protected function tearDown()
-    {
-        unlink(__DIR__.'/.env.testing');
-        unlink(__DIR__.'/.env.other');
-    }
-
     /** @test */
     public function can_read_file()
     {
+        file_put_contents(__DIR__.'/.env.testing', 'Sample content');
+        file_put_contents(__DIR__.'/.env.other', 'Other content');
         $adapter = new FileDotenvAdapter(__DIR__.'/.env.testing');
 
         $content = $adapter->read();
 
         $this->assertEquals("Sample content", $content);
+        unlink(__DIR__.'/.env.testing');
+        unlink(__DIR__.'/.env.other');
     }
 
     /** @test */
     public function can_write_file()
     {
+        file_put_contents(__DIR__.'/.env.testing', 'Sample content');
+        file_put_contents(__DIR__.'/.env.other', 'Other content');
         $adapter = new FileDotenvAdapter(__DIR__.'/.env.testing');
 
         $adapter->write("Updated content");
         $content = $adapter->read();
 
         $this->assertEquals('Updated content', $content);
+        unlink(__DIR__.'/.env.testing');
+        unlink(__DIR__.'/.env.other');
     }
 
     /** @test */
     public function two_instances_can_read_two_different_paths_at_the_same_time()
     {
+        file_put_contents(__DIR__.'/.env.testing', 'Sample content');
+        file_put_contents(__DIR__.'/.env.other', 'Other content');
         $adapterA = new FileDotenvAdapter(__DIR__.'/.env.testing');
         $adapterB = new FileDotenvAdapter(__DIR__.'/.env.other');
 
@@ -58,11 +48,15 @@ class FileDotenvAdapterTest extends TestCase
 
         $this->assertEquals('Sample content', $contentA);
         $this->assertEquals('Other content', $contentB);
+        unlink(__DIR__.'/.env.testing');
+        unlink(__DIR__.'/.env.other');
     }
 
     /** @test */
     public function two_instances_can_read_the_same_path_at_the_same_time()
     {
+        file_put_contents(__DIR__.'/.env.testing', 'Sample content');
+        file_put_contents(__DIR__.'/.env.other', 'Other content');
         $adapterA = new FileDotenvAdapter(__DIR__.'/.env.testing');
         $adapterB = new FileDotenvAdapter(__DIR__.'/.env.testing');
 
@@ -71,5 +65,7 @@ class FileDotenvAdapterTest extends TestCase
 
         $this->assertEquals('Sample content', $contentA);
         $this->assertEquals('Sample content', $contentB);
+        unlink(__DIR__.'/.env.testing');
+        unlink(__DIR__.'/.env.other');
     }
 }
