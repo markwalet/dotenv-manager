2a3aac0eb183ac96f748c4efb6bbeb35b4944571
= <m.walet@code14.nl>
2019-09-17 14:22:44 +0200

    Update testsuite to support phpunit 6.0

--- tests/DotenvManagerTest.php
+++ tests/DotenvManagerTest.php
@@ -12,35 +12,15 @@ use PHPUnit\Framework\TestCase;
 
 class DotenvManagerTest extends TestCase
 {
-    /**
-     * @var FakeDotenvAdapter
-     */
-    private $adapter;
-
-    /**
-     * @var DotenvManager
-     */
-    private $dotenv;
-
-
-    /**
-     * This method is called before each test.
-     */
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        $this->adapter = new FakeDotenvAdapter;
-        $this->dotenv = new DotenvManager($this->adapter);
-    }
-
     /** @test */
     public function can_add_a_line_to_dotenv()
     {
-        $this->adapter->setSource("TEST1=value");
+        $adapter = new FakeDotenvAdapter;
+        $dotenv = new DotenvManager($adapter);
+        $adapter->setSource("TEST1=value");
 
-        $this->dotenv->add('TEST2', 'value2');
-        $content = $this->adapter->read();
+        $dotenv->add('TEST2', 'value2');
+        $content = $adapter->read();
 
         $this->assertEquals("TEST1=value".PHP_EOL."TEST2=value2", $content);
     }
@@ -48,10 +28,12 @@ class DotenvManagerTest extends TestCase
     /** @test */
     public function can_create_a_line_in_dotenv()
     {
-        $this->adapter->setSource("TEST1=value");
+        $adapter = new FakeDotenvAdapter;
+        $dotenv = new DotenvManager($adapter);
+        $adapter->setSource("TEST1=value");
 
-        $this->dotenv->create('TEST2', 'value2');
-        $content = $this->adapter->read();
+        $dotenv->create('TEST2', 'value2');
+        $content = $adapter->read();
 
         $this->assertEquals("TEST1=value".PHP_EOL."TEST2=value2", $content);
     }
@@ -59,10 +41,12 @@ class DotenvManagerTest extends TestCase
     /** @test */
     public function can_set_a_line_in_dotenv()
     {
-        $this->adapter->setSource("TEST1=value");
+        $adapter = new FakeDotenvAdapter;
+        $dotenv = new DotenvManager($adapter);
+        $adapter->setSource("TEST1=value");
 
-        $this->dotenv->set('TEST1', 'value2');
-        $content = $this->adapter->read();
+        $dotenv->set('TEST1', 'value2');
+        $content = $adapter->read();
 
         $this->assertEquals("TEST1=value2", $content);
     }
@@ -70,10 +54,12 @@ class DotenvManagerTest extends TestCase
     /** @test */
     public function can_update_a_line_in_dotenv()
     {
-        $this->adapter->setSource("TEST1=value");
+        $adapter = new FakeDotenvAdapter;
+        $dotenv = new DotenvManager($adapter);
+        $adapter->setSource("TEST1=value");
 
-        $this->dotenv->update('TEST1', 'value2');
-        $content = $this->adapter->read();
+        $dotenv->update('TEST1', 'value2');
+        $content = $adapter->read();
 
         $this->assertEquals("TEST1=value2", $content);
     }
@@ -81,10 +67,12 @@ class DotenvManagerTest extends TestCase
     /** @test */
     public function can_delete_a_line_in_dotenv()
     {
-        $this->adapter->setSource("TEST1=value".PHP_EOL."TEST2=value2");
+        $adapter = new FakeDotenvAdapter;
+        $dotenv = new DotenvManager($adapter);
+        $adapter->setSource("TEST1=value".PHP_EOL."TEST2=value2");
 
-        $this->dotenv->delete('TEST1');
-        $content = $this->adapter->read();
+        $dotenv->delete('TEST1');
+        $content = $adapter->read();
 
         $this->assertEquals("TEST2=value2", $content);
     }
@@ -92,10 +80,12 @@ class DotenvManagerTest extends TestCase
     /** @test */
     public function can_unset_a_line_in_dotenv()
     {
-        $this->adapter->setSource("TEST1=value".PHP_EOL."TEST2=value2");
+        $adapter = new FakeDotenvAdapter;
+        $dotenv = new DotenvManager($adapter);
+        $adapter->setSource("TEST1=value".PHP_EOL."TEST2=value2");
 
-        $this->dotenv->unset('TEST1');
-        $content = $this->adapter->read();
+        $dotenv->unset('TEST1');
+        $content = $adapter->read();
 
         $this->assertEquals("TEST2=value2", $content);
     }
@@ -103,15 +93,17 @@ class DotenvManagerTest extends TestCase
     /** @test */
     public function can_mutate_multiple_lines_at_once_in_dotenv()
     {
-        $this->adapter->setSource("TEST1=value1".PHP_EOL."TEST2=value2".PHP_EOL."TEST3=value3");
+        $adapter = new FakeDotenvAdapter;
+        $dotenv = new DotenvManager($adapter);
+        $adapter->setSource("TEST1=value1".PHP_EOL."TEST2=value2".PHP_EOL."TEST3=value3");
 
-        $this->dotenv->mutate(function (DotenvBuilder $builder) {
+        $dotenv->mutate(function (DotenvBuilder $builder) {
             $builder->add('TEST4', 'escaped value');
             $builder->update('TEST2', 'updated')->after('TEST3');
             $builder->delete('TEST1');
             $builder->move('TEST4')->before('TEST3');
         });
-        $content = $this->adapter->read();
+        $content = $adapter->read();
 
         $this->assertEquals("TEST4=\"escaped value\"".PHP_EOL."TEST3=value3".PHP_EOL."TEST2=updated", $content);
     }
@@ -119,17 +111,19 @@ class DotenvManagerTest extends TestCase
     /** @test */
     public function can_extend_builder()
     {
-        $this->adapter->setSource("INTEGER_VALUE=111");
-        $methodsBefore = $this->dotenv->builder()->methods();
+        $adapter = new FakeDotenvAdapter;
+        $dotenv = new DotenvManager($adapter);
+        $adapter->setSource("INTEGER_VALUE=111");
+        $methodsBefore = $dotenv->builder()->methods();
         try {
-            $this->dotenv->extend('increment', Increment::class);
+            $dotenv->extend('increment', Increment::class);
         } catch (InvalidArgumentException $e) {
             $this->fail('Could not extend manager');
         }
-        $methodsAfter = $this->dotenv->builder()->methods();
+        $methodsAfter = $dotenv->builder()->methods();
 
-        $this->dotenv->increment('INTEGER_VALUE');
-        $content = $this->adapter->read();
+        $dotenv->increment('INTEGER_VALUE');
+        $content = $adapter->read();
 
         $this->assertArrayNotHasKey('increment', $methodsBefore);
         $this->assertArrayHasKey('increment', $methodsAfter);
@@ -139,23 +133,29 @@ class DotenvManagerTest extends TestCase
     /** @test */
     public function throws_an_exception_when_extending_with_a_non_class()
     {
+        $adapter = new FakeDotenvAdapter;
+        $dotenv = new DotenvManager($adapter);
         $this->expectException(InvalidArgumentException::class);
 
-        $this->dotenv->extend('name', 'no class');
+        $dotenv->extend('name', 'no class');
     }
 
     /** @test */
     public function throws_an_exception_when_extending_with_a_class_that_does_not_implement_change()
     {
+        $adapter = new FakeDotenvAdapter;
+        $dotenv = new DotenvManager($adapter);
         $this->expectException(InvalidArgumentException::class);
 
-        $this->dotenv->extend('name', InvalidBuilderChange::class);
+        $dotenv->extend('name', InvalidBuilderChange::class);
     }
 
     /** @test */
     public function can_get_the_builder_instance_from_the_manager()
     {
-        $result = $this->dotenv->builder();
+        $adapter = new FakeDotenvAdapter;
+        $dotenv = new DotenvManager($adapter);
+        $result = $dotenv->builder();
 
         $this->assertInstanceOf(DotenvBuilder::class, $result);
     }
