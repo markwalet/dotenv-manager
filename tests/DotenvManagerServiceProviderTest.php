dd3ae881a71eb70967cfb994ba35953e436e842e
Mark Walet <mark.walet@gmail.com>
2020-04-02 13:29:31 +0200

    Improve code coverage

--- tests/DotenvManagerServiceProviderTest.php
+++ tests/DotenvManagerServiceProviderTest.php
@@ -7,10 +7,13 @@ use MarkWalet\DotenvManager\DotenvManager;
 class DotenvManagerServiceProviderTest extends LaravelTestCase
 {
     /** @test */
-    public function it_binds_manager_to_the_application()
+    public function it_binds_a_manager_to_the_application()
     {
+        $this->app->registerConfiguredProviders();
         $bindings = $this->app->getBindings();
-
         $this->assertArrayHasKey(DotenvManager::class, $bindings);
+        
+        $result = $this->app->make(DotenvManager::class);
+        $this->assertInstanceOf(DotenvManager::class, $result);
     }
 }
